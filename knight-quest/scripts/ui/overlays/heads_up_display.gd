8c6d02a6cea55213d13144e6790d58f2ffbc2618
dev-laww <lawrenceallen.tech@gmail.com>
2025-06-17 21:39:55 +0800

    add heads-up display UI and script; set margins for MarginContainer

---
+++ knight-quest/scripts/ui/overlays/heads_up_display.gd
@@ -0,0 +1,10 @@
+extends MarginContainer
+class_name HeadsUpDisplay
+
+
+@onready var player_position: Marker2D = %PlayerPosition
+@onready var enemy_position: Marker2D = %EnemyPosition
+
+
+func _ready() -> void:
+    pass # Replace with function body.
