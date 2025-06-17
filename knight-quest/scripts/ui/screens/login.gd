7df83b81fe35660b22ec8e63214b61aa9cc9fba3
dev-laww <lawrenceallen.tech@gmail.com>
2025-06-17 20:50:41 +0800

    initial ui flow

---
+++ knight-quest/scripts/ui/screens/login.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+var scene: PackedScene = preload("res://scenes/ui/screens/main_menu.tscn")
+
+
+@onready var login_button: Button = %LoginButton
+
+
+func _ready() -> void:
+    login_button.pressed.connect(_on_login_button_pressed)
+
+
+func _on_login_button_pressed() -> void:
+    get_tree().change_scene_to_packed(scene)
