37e79d63c65139722c5cf6e2bf52c4051165e80e
dev-laww <lawrenceallen.tech@gmail.com>
2025-07-19 01:07:37 +0800

    feat: implemented level rendering

--- knight-quest/scripts/ui/screens/LevelSelect.cs
+++ knight-quest/scripts/ui/screens/LevelSelect.cs
@@ -10,11 +10,25 @@ public partial class LevelSelect : CanvasLayer
     [Export] private LevelInfo[] levels = [];
 
     [Node] private ResourcePreloader resourcePreloader;
+    [Node] private GridContainer levelsContainer;
 
     public override void _Notification(int what)
     {
         if (what != NotificationSceneInstantiated) return;
 
         WireNodes();
+    }
+
+    public override void _Ready()
+    {
+        foreach (var levelInfo in levels)
+        {
+            var level = resourcePreloader.InstanceSceneOrNull<Level>();
+
+            if (level == null) return;
+
+            level.LevelInfo = levelInfo;
+            levelsContainer.AddChild(level);
+        }
     }
 }
