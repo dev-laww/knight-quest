916d1d743abcce2948c900f8076997c421933e4b
dev-laww <lawrenceallen.tech@gmail.com>
2025-07-19 01:15:41 +0800

    feat: initial immediate delegate state machine

---
+++ knight-quest/scripts/utils/ImmediateDelegateStateMachine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace Game.Utils;
+
+public partial class ImmediateDelegateStateMachine : RefCounted
+{
+    // TODO: test this state machine implementation
+
+    public delegate void State();
+
+    private State currentState;
+    private readonly HashSet<State> states = [];
+
+    public void AddState(State state)
+    {
+        if (state == null) return;
+        states.Add(state);
+    }
+
+    public void AddStates(params State[] stateArray)
+    {
+        if (stateArray == null) return;
+
+        states.UnionWith(stateArray);
+    }
+
+    public void ChangeState(State toState)
+    {
+        if (toState == null || !states.Contains(toState)) return;
+        currentState = toState;
+        currentState();
+    }
+}
