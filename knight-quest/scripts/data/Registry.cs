173c965d37f58b2fc8275a3bea20781c3eed2bf0
dev-laww <lawrenceallen.tech@gmail.com>
2025-08-06 22:35:40 +0800

    feat: created base registry class

---
+++ knight-quest/scripts/data/Registry.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using FuzzySharp;
+using Game.Utils;
+using Godot;
+
+namespace Game.Data;
+
+#nullable enable
+public abstract partial class Registry<T, TRegistry> : RefCounted
+    where T : Resource
+    where TRegistry : Registry<T, TRegistry>,
+    new()
+{
+    protected static readonly Lazy<TRegistry> Instance = new(() => new TRegistry());
+
+    protected abstract string ResourcePath { get; }
+
+    protected static readonly Dictionary<string, T> Resources = [];
+
+    static Registry()
+    {
+        Instance.Value.LoadResources();
+    }
+
+    public static T? Get(string id)
+    {
+        Resources.TryGetValue(id, out var resource);
+
+        if (resource != null) return (T)resource.Duplicate();
+
+        var matches = Process.ExtractOne(id, [.. Resources.Keys]);
+
+        if (matches == null || matches.Score < 80) return null;
+
+        Resources.TryGetValue(matches.Value, out resource);
+
+        return resource?.Duplicate() as T;
+    }
+
+    public static bool Get(string id, out T? resource)
+    {
+        resource = Get(id);
+
+        return resource != null;
+    }
+
+    protected virtual void LoadResources()
+    {
+        var files = DirAccessUtils.GetFilesRecursively(Instance.Value.ResourcePath);
+
+        foreach (var path in files)
+        {
+            if (!path.EndsWith(".tres") && !path.EndsWith(".tres.remap")) continue;
+            
+
+            var resource = ResourceLoader.Load<T>(path);
+
+            if (resource == null) continue;
+
+            var fileName = path.GetFile();
+            var id = fileName.GetBaseName();
+
+            Resources[id] = resource;
+        }
+    }
+}
---
+++ knight-quest/scripts/utils/DirAccessUtils.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace Game.Utils;
+
+#nullable enable
+public static class DirAccessUtils
+{
+    public static List<string> GetFilesRecursively(string path, string? extension = null)
+    {
+        var files = new List<string>();
+
+        if (!DirAccess.DirExistsAbsolute(path))
+        {
+            GD.PrintErr($"Directory does not exist: {path}");
+            return [];
+        }
+
+        var dir = DirAccess.Open(path);
+
+        dir.ListDirBegin();
+
+        var file = dir.GetNext().TrimSuffix(".remap");
+
+        while (file != string.Empty)
+        {
+            if (DirAccess.DirExistsAbsolute($"{path}/{file}"))
+            {
+                files.AddRange(GetFilesRecursively($"{path}/{file}", extension) ?? []);
+            }
+            else if (extension == null || file.EndsWith(extension))
+            {
+                files.Add($"{path}/{file}");
+            }
+
+            file = dir.GetNext().TrimSuffix(".remap");
+        }
+
+        dir.ListDirEnd();
+
+        return files;
+    }
+
+    public static List<string> GetFilesFromDirectories(params string[] paths)
+    {
+        var files = new List<string>();
+
+        foreach (var path in paths)
+            files.AddRange(GetFilesRecursively(path) ?? []);
+
+        return files;
+    }
+}
