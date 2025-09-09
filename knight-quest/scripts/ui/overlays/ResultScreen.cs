4e9c4cdcaf3113838c4eb6cb8fd4fa9c4b9b3e32
crimson <ambilllKylee@gmail.com>
2025-09-09 17:58:14 +0800

    feat: add ResultScreen UI and functionality for displaying results and navigation

---
+++ knight-quest/scripts/ui/overlays/ResultScreen.cs
@@ -0,0 +1,68 @@
+using Godot;
+using System;
+using Game.Autoloads;
+using GodotUtilities;
+using GodotUtilities.Util;
+
+
+namespace Game.UI;
+
+[Scene]
+public partial class ResultScreen : CanvasLayer
+{
+
+    
+    [Node] private RichTextLabel resultLabel;
+    [Node] private Label rewardsLabel;
+    [Node] private Button menuButton;
+    [Node] private Button retryButton;
+    
+
+
+    public override void _Notification(int what)
+    {
+        if (what != NotificationSceneInstantiated) return;
+
+        WireNodes();
+    }
+
+    public override void _Ready()
+    {
+        menuButton.Pressed += OnMenuButtonPress;
+        retryButton.Pressed += OnRetryButtonPress;
+        
+    }
+
+    private void OnMenuButtonPress()
+    {
+
+        Navigator.Push("res://scenes/ui/screens/main_menu.tscn");
+        QueueFree();
+    }
+
+    private void OnRetryButtonPress()
+    {
+        Navigator.Push("res://scenes/ui/overlays/result_screen.tscn");
+        QueueFree();
+    }
+
+
+
+    public void ShowResult(bool victory)
+    {
+        if (victory)
+        {
+            resultLabel.Text = "Victory!";
+            resultLabel.AddThemeColorOverride("font_color", Colors.Green);
+            //TODO: add rewards to inventory && display
+            // int stars = GameSession.Instance.CurrentLevel.StarCount;
+            // rewardsLabel.Text = $"Stars earned: {stars}";
+        }
+        else
+        {
+            resultLabel.Text = "Defeat...";
+            resultLabel.AddThemeColorOverride("font_color", Colors.Red);
+            rewardsLabel.Text = "No stars earned.";
+        }
+    }
+}
