011af9204041bc73874ce6877e068818a8d59157
laww <lawrenceallen.tech@gmail.com>
2025-09-19 19:48:53 +0800

    updated structure to use class based routers

--- backend/app/routers/authentication.py
+++ backend/app/routers/authentication.py
@@ -1,4 +1,5 @@
 ﻿from fastapi import APIRouter, Request
+from fastapi_utils.cbv import cbv
 
 from ..utils.auth_providers import oauth
 from ..utils.jwt import *
@@ -11,23 +12,30 @@ router = APIRouter(
 google_router = APIRouter(prefix='/google')
 
 
-@google_router.get("/login")
-async def login(request: Request):
-    redirect_uri = request.url_for("auth_callback")
-    return await oauth.google.authorize_redirect(request, redirect_uri)
+@cbv(router)
+class Auth:
+    pass
 
 
-@google_router.get("/callback")
-async def auth_callback(request: Request):
-    token = await oauth.google.authorize_access_token(request)
+@cbv(google_router)
+class GoogleAuth:
+    @google_router.get("/login")
+    async def login(self, request: Request):
+        base_url = str(request.base_url).rstrip('/')
+        redirect_uri = f"{base_url}/auth/google/callback"
+        return await oauth.google.authorize_redirect(request, redirect_uri)
 
-    user_info = await oauth.google.userinfo(token=token)
-    token = encode(user_info)
+    @google_router.get("/callback", name="auth_callback")
+    async def auth_callback(self, request: Request):
+        token = await oauth.google.authorize_access_token(request)
 
-    return {
-        'token': token,
-        'user': user_info
-    }
+        user_info = await oauth.google.userinfo(token=token)
+        token = encode(user_info)
+
+        return {
+            'token': token,
+            'user': user_info
+        }
 
 
 router.include_router(google_router)
