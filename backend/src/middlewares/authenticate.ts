ff25ae45dfc182c0129f7e51f4387b6be4d7d165
laww <lawrenceallen.tech@gmail.com>
2025-09-29 10:56:25 +0800

    removed session id check

--- backend/src/middlewares/authenticate.ts
+++ backend/src/middlewares/authenticate.ts
@@ -4,9 +4,8 @@ import jwt, { Token } from '@utils/jwt'
 
 export const authenticate: RequestHandler = async (req, res, next) => {
     const token: string = req.cookies['access_token'] ?? req.headers['authorization']?.replace('Bearer ', '')
-    const sessionId: string = req.cookies['session_id'] ?? req.headers['x-session-id']
 
-    if (!token || !sessionId) res.respond(Response.unauthorized({ message: 'No valid session found. Please login again.' }))
+    if (!token) res.respond(Response.unauthorized({ message: 'No valid session found. Please login again.' }))
 
     const decoded = jwt.verifyToken<Token>(token)
 
