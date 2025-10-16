ac1cdbb92dfa282d74266ee783666bff60e4f2f7
laww <lawrenceallen.tech@gmail.com>
2025-10-16 20:36:42 +0800

    feat: implement deep link handling for login authentication

--- knight-quest/scripts/ui/screens/Login.cs
+++ knight-quest/scripts/ui/screens/Login.cs
@@ -3,60 +3,114 @@ using Game.Autoloads;
 using Game.Utils;
 using GodotUtilities;
 using Logger = Game.Utils.Logger;
+using System.Collections.Generic;
 
 namespace Game.UI;
 
 [Scene]
 public partial class Login : CanvasLayer
 {
-    [Node] private LineEdit usernameField;
-    [Node] private LineEdit passwordField;
-    [Node] private Button loginButton;
-    [Node] private Button registerButton;
-    [Node] private Button googleButton;
-    [Node] private Node deeplink;
+	[Node] private LineEdit usernameField;
+	[Node] private LineEdit passwordField;
+	[Node] private Button loginButton;
+	[Node] private Button registerButton;
+	[Node] private Button googleButton;
+	[Node] private Node deeplink;
 
-    [Export] private string googleAuthUrl;
+	[Export] private string googleAuthUrl;
 
-    public override void _Notification(int what)
-    {
-        if (what != NotificationSceneInstantiated) return;
+	public override void _Notification(int what)
+	{
+		if (what != NotificationSceneInstantiated) return;
 
-        WireNodes();
-    }
+		WireNodes();
+	}
 
-    public override void _Ready()
-    {
-        loginButton.Pressed += OnLoginPressed;
-        registerButton.Pressed += OnRegisterPressed;
-        googleButton.Pressed += OnGooglePressed;
+	public override void _Ready()
+	{
+		loginButton.Pressed += OnLoginPressed;
+		registerButton.Pressed += OnRegisterPressed;
+		googleButton.Pressed += OnGooglePressed;
 
-        deeplink.Call("initialize");
-    }
+		deeplink.Call("initialize");
+	}
 
-    private void OnDeepLinkReceived(RefCounted variant)
-    {
-        var link = new DeepLinkUrl(variant);
-        Logger.Debug($"Deep link received: {link.GetData()}");
-    }
+	private void OnDeepLinkReceived(RefCounted variant)
+	{
+		var link = new DeepLinkUrl(variant);
+		var data = link.GetData();
 
-    private void OnLoginPressed()
-    {
-        // var username = usernameField.Text;
-        // var password = passwordField.Text;
-        AudioManager.Instance.PlayClick();
-        Navigator.Push("res://scenes/ui/screens/main_menu.tscn");
-    }
+		if (data == null) return;
 
-    private void OnRegisterPressed()
-    {
-        AudioManager.Instance.PlayClick();
-        Navigator.Push("res://scenes/ui/screens/register.tscn");
-    }
+		Logger.Debug($"Deep link received: {data}");
 
-    private void OnGooglePressed()
-    {
-        AudioManager.Instance.PlayClick();
-        OS.ShellOpen(googleAuthUrl);
-    }
+		if (link.GetHost() == "login")
+		{
+			HandleLoginDeeplink(link);
+		}
+	}
+
+	private void HandleLoginDeeplink(DeepLinkUrl link)
+	{
+		var query = link.GetQuery();
+		Logger.Debug($"Login deeplink query: {query}");
+
+		var queryParams = ParseQueryString(query);
+
+		if (queryParams.TryGetValue("token", out var token) && !string.IsNullOrEmpty(token))
+		{
+			Logger.Debug($"JWT token found: {token}");
+
+			SaveManager.Data.Account.Token = token;
+
+			SaveManager.Save();
+
+			Logger.Info($"User authenticated successfully: {SaveManager.Data.Account.Username}");
+
+			Navigator.Push("res://scenes/ui/screens/main_menu.tscn");
+		}
+		else
+		{
+			Logger.Error("No token found in login deeplink");
+		}
+	}
+
+	private Dictionary<string, string> ParseQueryString(string query)
+	{
+		var result = new Dictionary<string, string>();
+
+		if (string.IsNullOrEmpty(query)) return result;
+
+		var pairs = query.Split('&');
+		foreach (var pair in pairs)
+		{
+			var keyValue = pair.Split('=', 2);
+			if (keyValue.Length == 2)
+			{
+				result[keyValue[0]] = keyValue[1];
+			}
+		}
+
+		return result;
+	}
+
+	private void OnLoginPressed()
+	{
+		// var username = usernameField.Text;
+		// var password = passwordField.Text;
+		AudioManager.Instance.PlayClick();
+		Navigator.Push("res://scenes/ui/screens/main_menu.tscn");
+	}
+
+	private void OnRegisterPressed()
+	{
+		AudioManager.Instance.PlayClick();
+		Navigator.Push("res://scenes/ui/screens/register.tscn");
+	}
+
+	private void OnGooglePressed()
+	{
+		AudioManager.Instance.PlayClick();
+		OS.ShellOpen(googleAuthUrl);
+	}
 }
